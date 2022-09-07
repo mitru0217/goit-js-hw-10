cbb87e019c8f337581a870801245b62f083d750e
Michael <mitru0217@gmail.com>
2022-09-07 23:37:00 +0300

    update

--- src/fetchCounties.js
+++ src/fetchCounties.js
@@ -1,8 +1,18 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 export default function fetchCountries(country) {
-  const response = fetch(
+  return fetch(
     `${BASE_URL}${country}?fields=name,capital,population,flags,languages`
-  ).then(res => res.json());
-  return response;
+  )
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(mistake);
+      }
+      return res.json();
+    })
+    .catch(error => {
+      console.log(error);
+      console.log('mistake');
+      Notify.failure(`❌ Oops, there is no country with that name`);
+    });
 }
--- src/index.js
+++ src/index.js
@@ -1,71 +1,77 @@
 import './css/styles.css';
+import cards from './templates/cards.hbs';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 import fetchCountries from './fetchCounties';
 
 const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
-
 function onInputChange(e) {
   e.preventDefault();
   const country = e.target.value;
-  fetchCountries(country).then(country => createMarkup(country));
+  console.log(country);
+  fetchCountries(country).then(country => {
+    createMarkup(country);
+  });
 }
 function createMarkup(obj) {
   console.log(obj);
-  const countryName = obj[0].name.official;
-  const flags = obj[0].flags.svg;
-  const capital = obj[0].capital[0];
-  const languages = Object.values(obj[0].languages).join(', ');
-  const population = obj[0].population;
-  console.log(flags);
-  console.log(countryName);
-  console.log(capital);
-  console.log(languages);
-  console.log(population);
-  `<div class="country__card">
+  return obj.map(
+    ({
+      name: { official },
+      flags: { svg },
+      capital,
+      languages,
+      population,
+    }) => {
+      // const countryName = official;
+      // const flags = svg;
+      // console.log(countryName);
+      // console.log(flags);
+      const capitalOfCountry = capital[0];
+      // console.log(capitalOfCountry);
+      // const populationOfCountry = population;
+      // console.log(populationOfCountry);
+      const languagesOfCountry = Object.values(languages).join(', ');
+      // console.log(languagesOfCountry);
+      return `<div class="country__card">
       <div class="country__head">
-      <img class="country__img" src="${flags}" alt="Flag" width="150" height="150">
-      <h1 class="country__title>${countryName}</h1>
+     <img class="country__img" src="${svg}" alt="Flag" width="150" height="150">
+     <h1 class="country__title>${official}</h1>
       </div>
-      <div class="country__info>
-      <h2>Capital:</h2>
-      <p>${capital}</p>
-      <h2> Population:</h2>
-      <p>${population}</p>
-      <h2>Languages:</h2>
-      <p>${languages}</p>
-      </div>
-      </div>`;
-
-  countryInfo.innerHTML = '';
-  countryList = markup;
+     <div class="country__info>
+    <h2>Capital:</h2>
+    <p>${capitalOfCountry}</p>
+    <h2> Population:</h2>
+    <p>${population}</p>
+    <h2>Languages:</h2>
+    <p>${languagesOfCountry}</p>
+    </div>
+    </div>`;
+    }
+  );
 }
 
-// HTTP-запросы
-// Используй публичный API Rest Countries, а именно ресурс name, возвращающий массив объектов стран удовлетворивших критерий поиска. Добавь минимальное оформление элементов интерфейса.
+// `<div class="country__card">
+//     <div class="country__head">
+//     <img class="country__img" src="${flags}" alt="Flag" width="150" height="150">
+//     <h1 class="country__title>${countryName}</h1>
+//     </div>
+//     <div class="country__info>
+//     <h2>Capital:</h2>
+//     <p>${capital}</p>
+//     <h2> Population:</h2>
+//     <p>${population}</p>
+//     <h2>Languages:</h2>
+//     <p>${languages}</p>
+//     </div>
+//     </div>`;
 
-// Напиши функцию fetchCountries(name) которая делает HTTP-запрос на ресурс name и возвращает промис с массивом стран - результатом запроса. Вынеси её в отдельный файл fetchCountries.js и сделай именованный экспорт.
-
-// Фильтрация полей
-// В ответе от бэкенда возвращаются объекты, большая часть свойств которых тебе не пригодится. Чтобы сократить объем передаваемых данных добавь строку параметров запроса - так этот бэкенд реализует фильтрацию полей. Ознакомься с документацией синтаксиса фильтров.
-
-// Тебе нужны только следующие свойства:
-
-// name.official - полное имя страны
-// capital - столица
-// population - население
-// flags.svg - ссылка на изображение флага
-// languages - массив языков
-// Поле поиска
-// Название страны для поиска пользователь вводит в текстовое поле input#search-box. HTTP-запросы выполняются при наборе имени страны, то есть по событию input. Но, делать запрос при каждом нажатии клавиши нельзя, так как одновременно получится много запросов и они будут выполняться в непредсказуемом порядке.
-
-// Необходимо применить приём Debounce на обработчике события и делать HTTP-запрос спустя 300мс после того, как пользователь перестал вводить текст. Используй пакет lodash.debounce.
-
-// Если пользователь полностью очищает поле поиска, то HTTP-запрос не выполняется, а разметка списка стран или информации о стране пропадает.
-
-// Выполни санитизацию введенной строки методом trim(), это решит проблему когда в поле ввода только пробелы или они есть в начале и в конце строки.
+// countryInfo.innerHTML = '';
+// countryList = markup;
